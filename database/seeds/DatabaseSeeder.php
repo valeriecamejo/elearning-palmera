428175237ca979f97cf03bf8e6924db7227d8780
Valerie Camejo <valerie260492@gmail.com>
2018-04-12 18:09:25 -0400

    Add seed for modules

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(BrandsTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
+        $this->call(ModulesTableSeeder::class);
     }
 }
