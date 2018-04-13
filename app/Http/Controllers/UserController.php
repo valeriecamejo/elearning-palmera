08494a51fbefc902e881bd045e6193570f3237c1
Solsiret Vasquez Santaella <aurivasquez2009@gmail.com>
2018-04-13 11:33:30 -0400

    add edit profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserProfileRequest;
 use Illuminate\Support\Facades\Session;
 use App\Brand;
 use App\Country;
