2bcd60adca5b70f0e2ec6253750ec97699744124
Solsiret Vasquez Santaella <aurivasquez2009@gmail.com>
2018-05-24 13:22:45 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
     @if(App::environment('production'))
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="{{ asset('js/configGlobal.js') }}"></script>
     @else
       {!! Html::script('js/vue/vue.js') !!}
       {!! Html::script('js/vue/axios.min.js') !!}
