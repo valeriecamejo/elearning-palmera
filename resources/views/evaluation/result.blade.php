ffc5a51ff875db063aef2744153973a149717220
Valerie Camejo <valerie260492@gmail.com>
2018-06-04 16:30:12 -0400

    new button to repeat evaluation

--- resources/views/evaluation/result.blade.php
+++ resources/views/evaluation/result.blade.php
@@ -14,7 +14,10 @@
         </div>
         <h5>Evaluación: @if($user_result->approved) Aprobada @else No Aprobada @endif
         </h5>
+        <div>
         <br><a class="btn btn-primary btn" href="{{ url('product/' . $product_id) }}">Regresar</a>
+        <a class="btn btn-primary btn" href="{{ url('evaluations/' . $evaluation->id . '/' . $evaluation->product_id) }}">Intentarlo de nuevo</a>
+        </div>
       </div>
     </div>
   </div>
